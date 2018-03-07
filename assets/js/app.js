a9931fb5ff5469fad30c0f3e924890589252a326
lzubiaur <zubiaur.laurent@gmail.com>
2018-03-08 00:57:18 +0100

    Update website

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+jQuery(document).foundation();
+
+//@prepros-prepend ../../foundation/bower_components/jquery/dist/jquery.min.js
+//@prepros-prepend ../../foundation/bower_components/what-input/dist/what-input.min.js
+//@prepros-prepend ../../foundation/bower_components/foundation-sites/dist/js/foundation.min.js
+//@prepros-append slick.min.js
